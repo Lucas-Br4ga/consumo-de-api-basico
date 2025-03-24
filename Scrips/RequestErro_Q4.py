068d4d69c0aba021241fa841c584e8882fd8fa93
Lucas Braga <lucasrbraga77@gmail.com>
2025-03-24 16:49:00 -0300

    modificação no RequestErro_Q4.py na mensagem de exception

--- Scrips/RequestErro_Q4.py
+++ Scrips/RequestErro_Q4.py
@@ -2,8 +2,8 @@ import requests
 r = requests.get("https://viacep.com.br/abc/")
 if (r.status_code == 200):
 
-    print(r)
     print("Json:", r.json())
 
 else:
+ print(r)
  print('Nao houve sucesso na requisicao.')
